4eef09852d77d49584c243e539ae741eb7c86b8a
takashi.suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-04-28 12:48:49 +0000

    enable cat to operate stdin

--- commands/cat/cat.c
+++ commands/cat/cat.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void do_cat(FILE *f);
+
 int main(int argc, char *argv[]) {
-    if (argc <= 1) {
-        fprintf(stderr, "%s: file name not give.\n", argv[0]);
-        exit(1);
-    }
+    if (argc == 1) {
+        do_cat(stdin);
+    } else {
+        for (int i = 1; i < argc; i++) {
+            FILE *file;
+            file = fopen(argv[i], "r");
+            
+            if (!file) {
+               perror(argv[i]);
+               exit(1);
+            }
     
-    for (int i = 1; i < argc; i++) {
-        FILE *file;
-        file = fopen(argv[i], "r");
-        
-        if (!file) {
-           perror(argv[i]);
-           exit(1);
+            do_cat(file);
+            fclose(file);
         }
-
-        int c;
-        while ((c = fgetc(file)) != EOF) {
-            if (putchar(c) < 0) exit(1);
-        }
-
-        fclose(file);
     }
-  
+
     exit(0);
+}
+
+static void do_cat(FILE *file) {
+    int c;
+    
+    while ((c = fgetc(file)) != EOF) {
+        if (putchar(c) < 0) exit(1);
+    }
 }
--- tests/commands/cat.sh
+++ tests/commands/cat.sh
@@ -9,3 +9,11 @@ assert_equal \
 assert_equal \
     "cat `dirname $0`/../index.sh `dirname $0`/../common.sh" \
     "`dirname $0`/../../commands/cat/bin/cat `dirname $0`/../index.sh `dirname $0`/../common.sh"
+
+assert_equal \
+    "cat < `dirname $0`/../index.sh" \
+    "`dirname $0`/../../commands/cat/bin/cat < `dirname $0`/../index.sh"
+
+assert_equal \
+    "cat < `dirname $0`/../index.sh < `dirname $0`/../common.sh" \
+    "`dirname $0`/../../commands/cat/bin/cat < `dirname $0`/../index.sh < `dirname $0`/../common.sh"
