d5981e6b7c3c0b907239ac1aed4134221d9c364c
takashi.suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-04-28 11:50:10 +0000

    use stdio.io in cat

--- commands/cat/cat.c
+++ commands/cat/cat.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-static void do_cat(const char *path);
-static void die(const char *s);
 
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
@@ -13,32 +8,21 @@ int main(int argc, char *argv[]) {
     }
     
     for (int i = 1; i < argc; i++) {
-        do_cat(argv[i]);
+        FILE *file;
+        file = fopen(argv[i], "r");
+        
+        if (!file) {
+           perror(argv[i]);
+           exit(1);
+        }
+
+        int c;
+        while ((c = fgetc(file)) != EOF) {
+            if (putchar(c) < 0) exit(1);
+        }
+
+        fclose(file);
     }
   
     exit(0);
-}
-
-static void do_cat(const char *path) {
-    int fd;
-
-    fd = open(path, O_RDONLY);
-    if (fd < 0) die(path);
-
-    char buf[1024];
-    int read_ret;
-
-    for(;;) {
-        read_ret = read(fd, buf, sizeof buf);
-        if (read_ret < 0) die(path);
-        if (read_ret == 0) break;
-        if (write(STDOUT_FILENO, buf, read_ret) < 0 ) die(path);
-    }
-
-    if (close(fd) < 0) die(path);
-}
-
-static void die(const char *s) {
-    perror(s);
-    exit(1);
 }
