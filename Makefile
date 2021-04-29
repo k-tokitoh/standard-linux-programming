543516d9d6b4f22586a0add79132a03c3a3201f2
takashi.suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-04-29 11:37:32 +0000

    enable 'make test'

---
+++ Makefile
@@ -0,0 +1,15 @@
+CC		= gcc
+CFLAGS	= -Wall
+SOURCES = $(wildcard commands/*/*.c)
+TARGETS = $(SOURCES:%.c=%)
+
+all: $(TARGETS)
+
+%: %.c
+	$(CC) $(CFLAGS) $< -o $@
+
+test: all
+	tests/index.sh
+
+clean:
+	rm $(TARGETS)
--- tests/commands/cat.sh
+++ tests/commands/cat.sh
@@ -4,16 +4,16 @@
 
 assert_equal \
     "cat `dirname $0`/../index.sh" \
-    "`dirname $0`/../../commands/cat/bin/cat `dirname $0`/../index.sh"
+    "`dirname $0`/../../commands/cat/cat `dirname $0`/../index.sh"
 
 assert_equal \
     "cat `dirname $0`/../index.sh `dirname $0`/../common.sh" \
-    "`dirname $0`/../../commands/cat/bin/cat `dirname $0`/../index.sh `dirname $0`/../common.sh"
+    "`dirname $0`/../../commands/cat/cat `dirname $0`/../index.sh `dirname $0`/../common.sh"
 
 assert_equal \
     "cat < `dirname $0`/../index.sh" \
-    "`dirname $0`/../../commands/cat/bin/cat < `dirname $0`/../index.sh"
+    "`dirname $0`/../../commands/cat/cat < `dirname $0`/../index.sh"
 
 assert_equal \
     "cat < `dirname $0`/../index.sh < `dirname $0`/../common.sh" \
-    "`dirname $0`/../../commands/cat/bin/cat < `dirname $0`/../index.sh < `dirname $0`/../common.sh"
+    "`dirname $0`/../../commands/cat/cat < `dirname $0`/../index.sh < `dirname $0`/../common.sh"
